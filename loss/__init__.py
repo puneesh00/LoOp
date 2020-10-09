43f9c6cf775e2097ee2eb502b69937ef8a1003dc
Puneesh Deora <puneesh6298@gmail.com>
2020-10-09 23:03:23 +0530

    Update __init__.py

--- loss/__init__.py
+++ loss/__init__.py
@@ -8,4 +8,4 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from .ml_losses import HPHNTripletLoss, LiftedStructureLoss, Npairloss
+from .ml_losses import HPHNTripletLoss, LiftedStructureLoss, Npairloss, MSloss
